d16cf8b1c9dff35e41ebef5bb54ad9cc3e828e65
Conor Johnston <cnrswim@yahoo.com>
2019-11-30 22:25:37 -0500

    added speech recognition to note creation

--- app.py
+++ app.py
@@ -18,9 +18,23 @@ class Note(BaseModel):
 
 db.create_tables([Note])
 
+def listener():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    try:
+        result = r.recognize_google(audio)
+        return result
+    except Exception:
+        print("Couldn't quite hear that.")
+
 def create_note():
-    new_title = input('Title: ')
-    new_body = input('Note: ')
+    print('Title: ')
+    new_title = listener()
+    print(new_title)
+    print('Body: ')
+    new_body = listener()
+    print(new_body)
     new_note = Note(title = new_title, body = new_body)
     new_note.save()
     print('\n******************')
@@ -59,20 +73,12 @@ def delete_note():
     selected = Note.get(Note.title == select)
     selected.delete_instance()
 
-def listener():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-    try:
-        print(r.recognize_google(audio))
-    except Exception:
-        print("Couldn't quite hear that.")
-
 def run_notetaker():
     print('\nWhat would you like to do?')
     choice = listener()
     #input('(new, edit title, edit body, edit note, list, delete): ')
     if choice == 'new':
+        print('new')
         create_note()
         run_notetaker()
     elif choice == 'edit title':
